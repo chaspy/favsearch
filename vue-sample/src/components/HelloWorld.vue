b90849e12b10f78a43c9def3bb55d70ebff64b71
kondo takeshi <chaspy@users.noreply.github.com>
2018-10-06 16:01:47 +0900

    vue sample

---
+++ vue-sample/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+  <p>{{title}}</p>
+  <!-- 子コンポーネントのmessageを設定して呼び出す -->
+  <hello-child message='メッセージだよ'/>
+ </div>
+</template>
+ 
+<script>
+import HelloChild from './HelloChild'
+export default {
+  data: function () {
+    return {
+      title: 'はろー！わーるど！'
+    }
+  },
+  components: {
+    HelloChild
+  }
+}
+</script>
