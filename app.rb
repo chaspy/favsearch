44817cf55409b6eb40555791e7c28d498be64d91
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-10-06 23:24:11 +0900

    Implement the preparation for twitter authentication

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require 'sinatra'
+require 'twitter'
+require 'omniauth'
+require 'omniauth-twitter'
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+enable :sessions
+
+# Twitter API initialization
+before do
+  @twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key        = YOUR_CONSUMER_KEY
+    config.consumer_secret     = YOUR_CONSUMER_SECRET
+    config.access_token        = nil
+    config.access_token_secret = nil
+  end
+end
+
+use Rack::Session::Cookie
+use OmniAuth::Builder do
+  provider :twitter, YOUR_CONSUMER_KEY, YOUR_CONSUMER_SECRET
+end
 
 get '/' do
   'hello, favsearch'
