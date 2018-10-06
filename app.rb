1fb10c0f181fed4e9938df5eae255cd8d68f4da8
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-10-06 23:24:45 +0900

    Implement the logic for twitter authentication

--- app.rb
+++ app.rb
@@ -23,5 +23,29 @@ use OmniAuth::Builder do
 end
 
 get '/' do
-  'hello, favsearch'
+  erb "<a href='/auth/twitter'>Login with Twitter</a><br>"
+end
+
+# Do Authentication
+get '/auth' do
+end
+
+# return Access Token
+get '/auth/:provider/callback' do
+  p 'START CALLBACK'
+  result = request.env['omniauth.auth']
+  pp result.credentials
+  @twitter.access_token = result.credentials.token
+  @twitter.access_token_secret = result.credentials.secret
+  result_fav = @twitter.favorites
+  fav_tweets = []
+
+  result_fav.each do |a|
+    fav_tweets.push(a.text)
+    puts a.text
+  end
+
+  erb "<a href='/'>Top</a><br>
+       <h1>#{params[:provider]}</h1>
+       <pre>#{JSON.pretty_generate(fav_tweets)}</pre>"
 end
