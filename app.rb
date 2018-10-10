cbac861683ab0723d95bfc7ba9d9aeb02187187b
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-10-10 22:32:11 +0900

    Add option parameter

--- app.rb
+++ app.rb
@@ -37,15 +37,15 @@ get '/auth/:provider/callback' do
   pp result.credentials
   @twitter.access_token = result.credentials.token
   @twitter.access_token_secret = result.credentials.secret
-  result_fav = @twitter.favorites
+  result_fav = @twitter.favorites(count: '200')
   fav_tweets = []
 
   result_fav.each do |a|
-    fav_tweets.push(a.text)
-    puts a.text
+    fav_tweets.push(a.full_text)
   end
 
   erb "<a href='/'>Top</a><br>
        <h1>#{params[:provider]}</h1>
+       <h2>Your fav count number: #{fav_tweets.count}</h2>
        <pre>#{JSON.pretty_generate(fav_tweets)}</pre>"
 end
